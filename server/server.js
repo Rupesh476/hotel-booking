8ceff8e2bc27419dd005048f7c56be9025d74738
Rupesh476 <sahrupesh288@gmail.com>
2025-05-22 10:08:07 +0545

    eight commit

--- server/server.js
+++ server/server.js
@@ -22,8 +22,7 @@ app.use(clerkMiddleware());
 //  Example test route
 app.get('/', (req, res) => res.send("API is working"));
 
-//  Other API routes (after body parser + auth)
-app.use("/api/some-other-route", yourOtherRouteHandler); // optional
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(` Server running on port ${PORT}`));
